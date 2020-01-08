0915f745cdfb4a0f5bf483f01c983b995b312d2b
ALPS-AA <ALPS-AA@10.32.28.40>
2020-01-08 14:36:32 +0900

    프로젝트 등록

---
+++ groovy-sampe-4/src/main/java/com/groovy/sample/Application.java
@@ -0,0 +1,21 @@
+/*
+/*
+ * Copyright (C) 2018-2019 Lotte Global Logistics Inc. 
+ * All rights reserved.
+ */
+package com.groovy.sample;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("\n\n");
+        System.out.println("############################################################");
+        System.out.println("#                                                          #");
+        System.out.println("#                 Run java application                     #");
+        System.out.println("#                      [For SVN]                           #");
+        System.out.println("#                                                          #");
+        System.out.println("############################################################");
+        System.out.println("\n\n");
+    }
+
+}
